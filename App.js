121700b8bf66fbb835e0a7556c26f20e996169f1
zebanNikolay <zebannikolay@gmail.com>
2017-09-24 18:32:02 +0900

    Done logo and switcher data

--- App.js
+++ App.js
@@ -11,19 +11,36 @@ function init() {
         controls: ["rulerControl", "typeSelector", "zoomControl", "geolocationControl"]
     });
 
+    initPlacemarks(sightseeingsPlacemarks);
 
-    sightseeingsPlacemarks.forEach(function (item) {
-        item.properties.balloonContentBody = [
-            '<address>',
-            '<img class="img" src="' + item.data.img + '"/>',
-            '<br/>',
-            item.properties.balloonContent,
-            '<br/>',
-            '<a href="http://lib-nogliki.muzkult.ru/img/upload/3368/documents/Putevoditel_Tropami_Noglik2_legkij.pdf">Скачать путеводитель</a>',
-            '</address>'
-        ].join('');
-        myMap.geoObjects.add(new ymaps.Placemark(item.coordinates, item.properties, options));
-    });
+    function initPlacemarks(placemarks) {
+        myMap.geoObjects.removeAll();
+        placemarks.forEach(function (item) {
+            item.properties.balloonContentBody = [
+                '<address>',
+                '<img class="img" src="' + item.data.img + '"/>',
+                '<br/>',
+                item.properties.balloonContent,
+                '<br/>',
+                '<a href="http://lib-nogliki.muzkult.ru/img/upload/3368/documents/Putevoditel_Tropami_Noglik2_legkij.pdf">Скачать путеводитель</a>',
+                '</address>'
+            ].join('');
+            myMap.geoObjects.add(new ymaps.Placemark(item.coordinates, item.properties, options));
+        });
+    }
 
+    var switchActive = function (button, placemarks) {
+        $(document).ready(function(){
+            button.click(function(){
+                $("div.active").removeClass("active");
+                $(this).addClass("active");
+                initPlacemarks(placemarks);
+            });
+        });
+
+    };
+
+    switchActive($(".sightseeings-button"), sightseeingsPlacemarks);
+    switchActive($(".street-button"), streetsPlacemarks);
 
 }
